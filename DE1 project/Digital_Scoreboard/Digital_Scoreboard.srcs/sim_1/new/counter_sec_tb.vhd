1d781206a8932a9c701015d527c3d03395d16a7d
martinmikes1 <147034090+martinmikes1@users.noreply.github.com>
2025-04-10 14:35:10 +0200

    Add files via upload
    Counter 90% finished 
    Top level is not finished

---
+++ DE1 project/Digital_Scoreboard/Digital_Scoreboard.srcs/sim_1/new/counter_sec_tb.vhd
@@ -0,0 +1,83 @@
+-- Testbench automatically generated online
+-- at https://vhdl.lapinoo.net
+-- Generation date : Thu, 10 Apr 2025 12:21:37 GMT
+-- Request id : cfwk-fed377c2-67f7b7d11ea43
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_counter_sec is
+end tb_counter_sec;
+
+architecture tb of tb_counter_sec is
+
+    component counter_sec
+        port (clk      : in std_logic;
+              rst      : in std_logic;
+              en       : in std_logic;
+              count    : out std_logic_vector (3 downto 0);
+              count_10 : out std_logic_vector (2 downto 0);
+              min      : out std_logic_vector (3 downto 0);
+              min_10   : out std_logic_vector (3 downto 0));
+    end component;
+
+    signal clk      : std_logic;
+    signal rst      : std_logic;
+    signal en       : std_logic;
+    signal count    : std_logic_vector (3 downto 0);
+    signal count_10 : std_logic_vector (2 downto 0);
+    signal min      : std_logic_vector (3 downto 0);
+    signal min_10   : std_logic_vector (3 downto 0);
+
+    constant TbPeriod : time := 1 ns; -- ***EDIT*** Put right period here
+    signal TbClock : std_logic := '0';
+    signal TbSimEnded : std_logic := '0';
+
+begin
+
+    dut : counter_sec
+    port map (clk      => clk,
+              rst      => rst,
+              en       => en,
+              count    => count,
+              count_10 => count_10,
+              min      => min,
+              min_10   => min_10);
+
+    -- Clock generation
+    TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
+
+    -- ***EDIT*** Check that clk is really your main clock signal
+    clk <= TbClock;
+
+    stimuli : process
+    begin
+        -- EDIT Adapt initialization as needed
+        en <= '1';
+
+        -- Reset generation
+        -- EDIT: Check that rst is really your reset signal
+        rst <= '1';
+        wait for 10 ns;
+        rst <= '0';
+        wait for 10 ns;
+
+        -- EDIT Add stimuli here
+        wait for 1000 * TbPeriod;
+        
+        en <= '0';
+        wait for 21 * TbPeriod;
+
+        -- Stop the clock and hence terminate the simulation
+        TbSimEnded <= '1';
+        wait;
+    end process;
+
+end tb;
+
+-- Configuration block below is required by some simulators. Usually no need to edit.
+
+configuration cfg_tb_counter_sec of tb_counter_sec is
+    for tb
+    end for;
+end cfg_tb_counter_sec;
---
+++ DE1 project/Digital_Scoreboard/Digital_Scoreboard.srcs/sources_1/new/counter_sec.vhd
@@ -0,0 +1,84 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/10/2025 01:11:03 PM
+-- Design Name: 
+-- Module Name: counter_sec - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity counter_sec is
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           en : in STD_LOGIC;
+           count : out STD_LOGIC_VECTOR (3 downto 0);
+           count_10: out STD_LOGIC_VECTOR (2 downto 0);
+           min: out STD_LOGIC_VECTOR (3 downto 0);
+           min_10: out STD_LOGIC_VECTOR (3 downto 0)
+           );
+end counter_sec;
+
+architecture Behavioral of counter_sec is
+    signal sig_cnt: std_logic_vector (3 downto 0);
+    signal sig_cnt_10: std_logic_vector (2 downto 0);
+    signal sig_min: std_logic_vector (3 downto 0);
+    signal sig_min_10: std_logic_vector (3 downto 0);
+begin
+    process (clk)
+begin
+   if rising_edge(clk) then
+      if rst ='1' then
+         sig_cnt <= (others => '0');
+         sig_cnt_10 <= (others => '0');
+         sig_min <= (others => '0');
+         sig_min_10 <= (others => '0');
+      elsif en ='1' then
+         sig_cnt <= sig_cnt + 1;
+         if sig_cnt = x"9" then 
+            sig_cnt <= (others => '0');
+            sig_cnt_10 <= sig_cnt_10 + 1;
+            if sig_cnt_10 = x"5" then 
+                sig_cnt_10 <= (others => '0');
+                sig_min <= sig_min +1;
+                if sig_min =x"9" then 
+                sig_min <= (others => '0');
+                sig_min_10 <= sig_min_10 +1;
+                end if;
+            end if;
+        end if; 
+      end if;
+   end if;
+   
+
+end process;
+
+count <= sig_cnt;
+count_10 <= sig_cnt_10;
+min <= sig_min;
+min_10 <= sig_min_10;
+end Behavioral;
+
