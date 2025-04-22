662def457eca220ef44692a020146ed8c0679ae1
baland1 <146985837+baland1@users.noreply.github.com>
2025-04-22 17:29:54 +0200

    Add files via upload

---
+++ DE1 project/Digital_Scoreboard/Digital_Scoreboard/Digital_Scoreboard.srcs/sources_1/new/top_level.vhd
@@ -0,0 +1,53 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/10/2025 02:30:21 PM
+-- Design Name: 
+-- Module Name: top_level - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top_level is
+    Port ( CLK100MHZ : in STD_LOGIC;
+           BTNC : in STD_LOGIC;
+           CA : out STD_LOGIC;
+           CB : out STD_LOGIC;
+           CC : out STD_LOGIC;
+           CD : out STD_LOGIC;
+           CE : out STD_LOGIC;
+           CF : out STD_LOGIC;
+           CG : out STD_LOGIC;
+           DP : out STD_LOGIC;
+           AN : out STD_LOGIC_VECTOR (7 downto 0));
+end top_level;
+
+architecture Behavioral of top_level is
+
+begin
+
+
+end Behavioral;
